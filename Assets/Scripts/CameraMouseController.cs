69a8aee08ff3975b695a5bba13f76bcbafaa7b4d
Owen Morgan <om371@bath.ac.uk>
2017-06-02 15:57:18 +0100

    Added more green screen videos, buttons.

---
+++ Assets/Scripts/CameraMouseController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseController : MonoBehaviour {
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float movesensitivityX = 1f;
+	public float movesensitivityY = 1f;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	public float speed = 0.1f;
+	float rotationX = 0F;
+	float rotationY = 0F;
+	Quaternion originalRotation;
+	Rigidbody rb;
+	Transform transform;
+	void Update (){
+		rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+		rotationX = ClampAngle (rotationX, minimumX, maximumX);
+		rotationY = ClampAngle (rotationY, minimumY, maximumY);
+		Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+		Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
+		transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 direction = transform.right*moveHorizontal*movesensitivityX + transform.forward*moveVertical*movesensitivityY;
+		if(rb == null){
+			direction = direction + transform.localPosition;
+			transform.localPosition = new Vector3(direction.x,transform.localPosition.y,direction.z);
+		}
+		else{
+			rb.AddForce(direction*speed);
+		}
+		
+	}
+
+	void Start(){
+		transform = gameObject.GetComponent<Transform>();
+		rb = gameObject.GetComponent<Rigidbody>();
+		originalRotation = transform.localRotation;
+	}
+	public static float ClampAngle (float angle, float min, float max){
+		if (angle < -360F){
+			angle += 360F;
+		}
+		if (angle > 360F){
+			angle -= 360F;
+		}
+		return Mathf.Clamp (angle, min, max);
+	}
+}
---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour {
+
+	
+	// Use this for initialization
+	void Start () {
+		Transform t = gameObject.GetComponent<Transform>();
+		VideoPlayer vp = gameObject.GetComponent<VideoPlayer>();
+		vp.clip = Resources.Load("Leonardo") as VideoClip;
+		print(vp.clip);
+		VideoClip clip = vp.clip;
+		float aspectRatio = (float)clip.width/(float)clip.height;
+		t.localScale = new Vector3(t.localScale.y * aspectRatio, t.localScale.y,t.localScale.z);
+	}
+}
