ee302195ace39265356dc56912022a8369e6b4b5
Owen Morgan <o.morgan@bath.ac.uk>
2017-06-28 15:19:09 +0100

    Added question mark/tutorial code.

---
+++ Assets/Scripts/TutorialPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPlayer : MonoBehaviour, IInteractive {
+
+	public VideoController vc;
+
+	//this function must be defined to satisfy the interface.
+	//the tutorial video doesn't exist yet though: hence it does nothing.
+	public void GVRClick(){
+		//vc.PlayVideo("Tutorial");
+	}
+
+}
